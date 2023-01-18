6e686462894038d3ec3bc4bf8a2f56548dd87ff6
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-18 12:32:08 -0800

    add save function with save entries to people counter

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 let count = 0;
-let countEl = document.getElementById("count-el")
+let countEl = document.getElementById("count-el");
+let prev = document.getElementById("prev");
 
 function increment() {
     count += 1;
@@ -11,4 +12,10 @@ function decrement() {
         count -= 1;
         countEl.innerHTML = count;
     }
+}
+
+function save() {
+    prev.textContent += count + " - ";
+    count = 0;
+    countEl.textContent = count;
 }
