40e73e5c9d95b078c486823a86e172970b786ee7
RaymondwChen2 <raymondwchen2@gmail.com>
2023-01-18 15:43:16 -0800

    started a calculator

--- index.js
+++ index.js
@@ -1,21 +1,27 @@
 
-let count = 0;
-let countEl = document.getElementById("count-el");
-let prev = document.getElementById("prev");
+// let count = 0;
+// let countEl = document.getElementById("count-el");
+// let prev = document.getElementById("prev");
 
-function increment() {
-    count += 1;
-    countEl.innerHTML = count;
-}
-function decrement() {
-    if (count !== 0) {
-        count -= 1;
-        countEl.innerHTML = count;
-    }
-}
+// function increment() {
+//     count += 1;
+//     countEl.innerHTML = count;
+// }
+// function decrement() {
+//     if (count !== 0) {
+//         count -= 1;
+//         countEl.innerHTML = count;
+//     }
+// }
 
-function save() {
-    prev.textContent += count + " - ";
-    count = 0;
-    countEl.textContent = count;
-}
+// function save() {
+//     prev.textContent += count + " - ";
+//     count = 0;
+//     countEl.textContent = count;
+// }
+
+let num = 0;
+
+let four = document.getElementById("btn-4");
+
+
